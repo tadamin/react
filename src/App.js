f93a7a73e09c64b59566cd0b45d4b0ca2c4cbf5e
tadamin <50764232+tadamin@users.noreply.github.com>
2020-02-20 17:13:40 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState(1)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,9 +20,13 @@ function App() {
         >
           Learn React
         </a>
+        <div>{count}</div>
+        <div>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
